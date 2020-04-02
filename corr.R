1fbd2eaf53b403e134c0d883316f4fb4172dba4b
cuddjim <jimmy.cuddihy@gmail.com>
2020-04-02 13:09:51 -0400

    week 2 assignment 1

--- corr.R
+++ corr.R
@@ -6,10 +6,28 @@ setwd('specdata')
 pollutant_files <-  suppressWarnings(map_df(list.files(pattern = '*.csv') %>% 
                                               lapply(read.csv),data.frame) %>% 
                                        clean_names())
-t <- complete(1:332)  
-poll_noNA <- pollutant_files %>% 
-    mutate(nobs = ifelse(!is.na(sulfate) & !is.na(nitrate),1,0)) %>% 
-  filter(nobs > 0) %>% select(-nobs) %>% left_join(t, by = 'id') %>% 
-  set_colnames(c('date','sulfate','nitrate','id','ncomplete'))
 
-cor(poll_noNA$sulfate,poll_noNA$nitrate)
+#from complete.R
+t <- complete(1:332)  
+
+
+#join complete cases to t to show how many complete cases per id, filter by threshold
+#apply cor function to each remaining id
+corr <- function(threshold) {
+  poll_noNA_test <- pollutant_files %>% 
+    mutate(nobs = ifelse(!is.na(sulfate) & !is.na(nitrate),1,0)) %>% 
+    filter(nobs > 0) %>% select(-nobs) %>% left_join(t, by = 'id') %>% filter(nobs > threshold)
+  sapply(unique(poll_noNA_test$id), function(x) {
+    poll_noNA_test %<>%  filter(id == x)
+    l <- unlist(list(cor(poll_noNA_test$sulfate,poll_noNA_test$nitrate)))
+  })}
+
+#test
+test <- corr(150)
+head(test)
+summary(test)
+
+
+
+
+
